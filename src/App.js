fa74988128621e7c87c1294aa00e4e32a81077d3
Konstantin Travin <travin.kos@yandex.ru>
2019-10-09 22:44:24 +0300

    Cоздание проекта, настройка webpack

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout';
+
+
+
+function App() {
+  return (
+    <Layout>
+      <div style={{width: '400px', border: '1px solid #000'}}>
+        <h1>Layout works</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
