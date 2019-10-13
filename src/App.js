42dcada86e98eade60b855ff809b12eae72302a7
Konstantin Travin <travin.kos@yandex.ru>
2019-10-13 16:35:15 +0300

    Готовый квиз, установка и настрой роутинга

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz'
 
 
 
 function App() {
   return (
     <Layout>
-      <div style={{width: '400px', border: '1px solid #000'}}>
-        <h1>Layout works</h1>
-      </div>
+        <Quiz></Quiz>
     </Layout>
   );
 }
